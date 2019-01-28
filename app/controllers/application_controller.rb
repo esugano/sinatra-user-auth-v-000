e41e93151f629e910ef41377cb6228306eab836a
Emily S <emily.sugano@gmail.com>
2019-01-28 22:02:28 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,5 @@
+require_relative '../config/environment'
+
 class ApplicationController < Sinatra::Base
   register Sinatra::ActiveRecordExtension
   set :views, Proc.new { File.join(root, "../views/") }
