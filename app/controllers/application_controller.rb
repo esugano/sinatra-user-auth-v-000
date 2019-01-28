efe4e4b14a9bee59bdc45ee694ddd2c05a358e99
Emily S <emily.sugano@gmail.com>
2019-01-28 22:04:26 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,11 @@
-require_relative '../config/environment'
+
+require './config/environment'
 
 class ApplicationController < Sinatra::Base
-  register Sinatra::ActiveRecordExtension
-  set :views, Proc.new { File.join(root, "../views/") }
 
   configure do
+    set :public_folder, 'public'
+    set :views, 'app/views'
     enable :sessions
     set :session_secret, "secret"
   end
